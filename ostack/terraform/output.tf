5cc65a35327e445c9c58f32170050ada5a1ae694
Gianni Dalla Torre <gianni@ebi.ac.uk>
2018-08-02 15:21:28 +0100

    Expose k8s master private ip

--- ostack/terraform/output.tf
+++ ostack/terraform/output.tf
@@ -1,3 +1,7 @@
 output "external_ip" {
   value = "${openstack_networking_floatingip_v2.floatingip.address}"
 }
+
+output "k8s-master_private_ip" {
+  value = "${openstack_compute_instance_v2.k8s-master.*.access_ip_v4[0]}"
+}
